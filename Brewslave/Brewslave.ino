511b9fc118c4abc15a13bffa9ca96354829dce56
Philipp Bender <philipp@rootiniert.de>
2013-12-22 18:11:12 +0100

    experiments with matrices

--- Brewslave/Brewslave.ino
+++ Brewslave/Brewslave.ino
@@ -1,24 +1,70 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
+#include <MatrixMath.hpp>
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+float Q     [2][2];
+float R     [2][2];
+float K     [2][2];
+float H     [2][1];
+float P_k_m [2][2];
+float P_k   [2][2];
+float x_k_m [2][1];
+float x_k   [2][1];
+float F     [2][2];
+
+
+unsigned long time;
+
+void setup() {
+    Serial.begin(9600); 
+
+    // constant slope
+    F[0][0] = 1;
+    F[1][0] = 0;
+    F[0][1] = 1;
+    F[1][1] = 1;
+    
+    R[0][0] = 1;
+    R[1][0] = 0;
+    R[0][1] = 0;
+    R[1][1] = 1;
+    
+    P_k[0][0] = 1;
+    P_k[1][0] = 0;
+    P_k[0][1] = 0;
+    P_k[1][1] = 1;
+
+    x_k[0][0] = 0;
+    x_k[1][0] = 0;
+    
+    Q[0][0] = 1;
+    Q[1][0] = 0;
+    Q[0][1] = 0;
+    Q[1][1] = 1;
+
+    H[0][0] = 1;
+    H[1][0] = 0;
+    H[0][1] = 0;
+    H[1][1] = 0;
+
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);               // wait for a second
+float slope   = 1.3;
+float x_meas  = 10;
+
+void loop(){
+
+    time = millis();
+
+    // run process
+    x_meas += slope;
+
+    // Kalman I: predict
+    Matrix.Multiply(x_k, F, 2, 2, 1, x_k_m);
+
+
+    unsigned long time_new = millis();
+    Serial.print("Took: ");
+    Serial.print(time_new - time);
+    Serial.print("\n");
+    
+
 }

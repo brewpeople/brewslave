90077ab6aa757b18fccbe2fb78c89d9c9e1247ba
Philipp Bender <philippbender4@gmail.com>
2013-12-25 19:14:17 +0100

    made code a little bit more expressive

---
+++ Brewslave/brew_control_protocol.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace brewmeister
+{
+    enum COMMAND
+    {
+        READ = 0xf0,
+        WRITE = 0xf1
+    };
+
+    enum INSTRUMENT
+    {
+        TEMP = 0xf1,
+        HEAT = 0xf2,
+        STIR = 0xf3
+    };
+
+}
+
+
+
